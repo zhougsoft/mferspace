1c72ca7e899a7b9827dc8121cf8c91a910c159d0
zhoug <90252209+zhoug0x@users.noreply.github.com>
2022-12-03 07:10:50 -0400

    fix: swapped out old env var for node urls

--- services/mfers.ts
+++ services/mfers.ts
@@ -1,46 +1,54 @@
-import { ethers } from 'ethers';
-import abi from '../config/abi/mfers.json';
-import { Mfer } from '../interfaces';
-import { MFER_CONTRACT_ADDRESS, IPFS_GATEWAY } from '../config/constants';
+import { ethers } from 'ethers'
+import abi from '../config/abi/mfers.json'
+import { Mfer } from '../interfaces'
+import { MFER_CONTRACT_ADDRESS, IPFS_GATEWAY } from '../config/constants'
 
-// For handling all mfers data fetching logic
+// reads .env for API keys and returns a URL for available nodes
+function getNodeUrl() {
+  const { ALCHEMY_ID, INFURA_ID } = process.env
+  if (!ALCHEMY_ID && !INFURA_ID) {
+    throw Error('no Alchemy or Infura API keys found in .env')
+  }
+
+  // if no alchemy API key, use infura
+  if (ALCHEMY_ID) return `https://eth-mainnet.g.alchemy.com/v2/${ALCHEMY_ID}`
+  return `https://eth-mainnet.g.alchemy.com/v2/${ALCHEMY_ID}`
+}
 
 // Connect to mfers contract via RPC
-const _getMfersContract = () => {
-	const provider = new ethers.providers.JsonRpcProvider(
-		process.env.ETHEREUM_NODE_URL
-	);
-	return new ethers.Contract(MFER_CONTRACT_ADDRESS, abi, provider);
-};
+function getMfersContract() {
+  const provider = new ethers.providers.JsonRpcProvider(getNodeUrl())
+  return new ethers.Contract(MFER_CONTRACT_ADDRESS, abi, provider)
+}
 
 // Get data for a single mfer by id
-export const getMfer = async (id: number): Promise<Mfer> => {
-	// Fetch mfer tokenURI
-	const contract = _getMfersContract();
-	const tokenURI: string = await contract.tokenURI(id);
+export async function getMfer(id: number): Promise<Mfer> {
+  // Fetch mfer tokenURI
+  const contract = getMfersContract()
+  const tokenURI: string = await contract.tokenURI(id)
 
-	// Build IPFS gateway URL from IPFS content identifier hash
-	const uriSplit = tokenURI.split('/');
-	const ipfsContentId = uriSplit[2];
-	const tokenId = uriSplit[3];
-	const tokenIpfsGateway = `${IPFS_GATEWAY}/${ipfsContentId}/${tokenId}`;
+  // Build IPFS gateway URL from IPFS content identifier hash
+  const uriSplit = tokenURI.split('/')
+  const ipfsContentId = uriSplit[2]
+  const tokenId = uriSplit[3]
+  const tokenIpfsGateway = `${IPFS_GATEWAY}/${ipfsContentId}/${tokenId}`
 
-	// Fetch mfer image data from IPFS & build a gateway link for the image
-	const mferResult = await fetch(tokenIpfsGateway).then(res => res.json());
-	const imgIpfsHash = mferResult.image.split('/')[2];
-	const img = `${IPFS_GATEWAY}/${imgIpfsHash}`;
+  // Fetch mfer image data from IPFS & build a gateway link for the image
+  const mferResult = await fetch(tokenIpfsGateway).then(res => res.json())
+  const imgIpfsHash = mferResult.image.split('/')[2]
+  const img = `${IPFS_GATEWAY}/${imgIpfsHash}`
 
-	return {
-		id,
-		name: mferResult.name,
-		img,
-		attributes: mferResult.attributes,
-	};
-};
+  return {
+    id,
+    name: mferResult.name,
+    img,
+    attributes: mferResult.attributes,
+  }
+}
 
 // Get the address holding a specific mfer id
-export const getMferOwner = async (id: number): Promise<string> => {
-	const contract = _getMfersContract();
-	const mferOwner = contract.ownerOf(id);
-	return mferOwner;
-};
+export async function getMferOwner(id: number): Promise<string> {
+  const contract = getMfersContract()
+  const mferOwner = contract.ownerOf(id)
+  return mferOwner
+}
