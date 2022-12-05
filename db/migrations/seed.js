7758bc7759b36853579f9a8974c1e216d70c6ca4
zhoug <90252209+zhoug0x@users.noreply.github.com>
2022-12-05 19:34:17 -0400

    chore: update db seed script to fill all possible profile fields for example

--- db/migrations/seed.js
+++ db/migrations/seed.js
@@ -4,7 +4,7 @@ withSql(async sql => {
   console.log('seeding db with test data...')
 
   await sql`
-    UPDATE profiles SET name='zhoug', tagline='sup mfers', bio_about='just another mfer', bio_meet='chill mfers'
+    UPDATE profiles SET name='♥ zhoug ♥', tagline='sup mfersss ♡ *~~~', gender='he/mfer', age='420 yrs old', location='bing bong, canada', song_url='https://soundcloud.com/panicatthedisco/i-write-sins-not-tragedies', bio_about='heyyy mfersss <3 im zhoug. i like to buidl and chill and stuffff ♥', bio_meet='♡ chill mfers who be buidlin ♡'
     WHERE mfer_id=3191
   `
 
