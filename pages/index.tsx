330a83fac5e660d403e1ebdc8a5eca33329e7b26
zhoug <90252209+zhoug0x@users.noreply.github.com>
2022-12-18 12:22:58 -0400

    feat: homepage responsiveness

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,59 +1,11 @@
 import Link from 'next/link'
+import Image from 'next/image'
 import styled from 'styled-components'
 import type { Profile } from '../interfaces'
 import { readAll as getProfiles } from '../services/profiles'
 import { makeRandomKey } from '../utils'
 import { Container, IconEmoji } from '../components/Shared'
 import Layout from '../components/Layout'
-
-const ProfileCardWrapper = styled.div`
-  position: relative;
-  z-index: 1;
-  text-align: right;
-  border: 1px solid ${({ theme }) => theme.colors.blue};
-  border-radius: 5px;
-  color: ${({ theme }) => theme.colors.text};
-  padding: 0.5rem 1rem;
-  margin-bottom: 1rem;
-  transition: 0.2s;
-
-  &:hover {
-    transform: scale(1.01);
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-  }
-
-  @media (min-width: ${({ theme }) => theme.breakpoints.md}px) {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    text-align: left;
-  }
-`
-
-const ProfileInfoWrapper = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  z-index: 3;
-
-  & > .profile-info-head {
-    width: 10rem;
-    margin-right: 1rem;
-
-    @media (min-width: ${({ theme }) => theme.breakpoints.md}px) {
-      width: 7rem;
-      margin-right: 1rem;
-    }
-  }
-  
-  & > .profile-info-name {
-    font-size: 1.6rem;
-    @media (min-width: ${({ theme }) => theme.breakpoints.md}px) {
-      font-size: 1.45rem;
-      
-    }
-  }
-`
 
 const MferImgAccent = styled.img`
   display: none;
@@ -72,23 +24,87 @@ const MferImgAccent = styled.img`
   }
 `
 
+const ProfileInfoWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  z-index: 3;
+
+  div.profile-info-head {
+    width: 10rem;
+    margin-right: 1rem;
+
+    & > img {
+      width: 100%;
+      min-width: 10rem;
+      @media (min-width: ${({ theme }) => theme.breakpoints.md}px) {
+        min-width: unset;
+      }
+    }
+
+    @media (min-width: ${({ theme }) => theme.breakpoints.md}px) {
+      width: 7rem;
+      margin-right: 1rem;
+      min-width: unset;
+    }
+  }
+
+  div.profile-info-name {
+    font-size: 1.6rem;
+
+    & > h3 {
+      margin: 0;
+    }
+
+    @media (min-width: ${({ theme }) => theme.breakpoints.md}px) {
+      font-size: 1.45rem;
+    }
+  }
+`
+
 function ProfileInfo({ profile }: { profile: Profile }) {
   return (
     <ProfileInfoWrapper>
       <div className="profile-info-head">
         <img
-          style={{ width: '100%' }}
           src={`https://heads.mfers.dev/${profile.mfer_id}.png`}
           alt={`the head of mfer #${profile.mfer_id}`}
         />
       </div>
       <div className="profile-info-name">
-        <h3 style={{ margin: 0 }}>{profile.name}</h3>
+        <h3>{profile.name}</h3>
         <small>{`mfer #${profile.mfer_id}`}</small>
       </div>
     </ProfileInfoWrapper>
   )
 }
+
+const ProfileCardWrapper = styled.div`
+  position: relative;
+  z-index: 1;
+  text-align: right;
+  max-width: 60rem;
+
+  border: 1px solid ${({ theme }) => theme.colors.blue};
+  border-radius: 5px;
+  color: ${({ theme }) => theme.colors.text};
+  padding: 0rem 2rem 0 0;
+  margin-bottom: 1rem;
+  transition: 0.2s;
+
+  &:hover {
+    transform: scale(1.01);
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  }
+
+  @media (min-width: ${({ theme }) => theme.breakpoints.md}px) {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    text-align: left;
+    padding: 0.5rem 1rem;
+  }
+`
 
 function ProfileCard({ profile }: { profile: Profile }) {
   return (
@@ -101,32 +117,34 @@ function ProfileCard({ profile }: { profile: Profile }) {
   )
 }
 
+const HomeHeaderWrapper = styled.div`
+  margin-bottom: 2rem;
+
+  h1.header-title {
+    font-size: 3rem;
+    margin: 0 0.5rem 0 0;
+    @media (min-width: ${({ theme }) => theme.breakpoints.lg}px) {
+      font-size: 4rem;
+    }
+  }
+
+  div.header-subtext {
+    font-size: 1.5rem;
+    letter-spacing: -0.05em;
+    @media (min-width: ${({ theme }) => theme.breakpoints.lg}px) {
+      font-size: 2.5rem;
+    }
+  }
+`
+
 function HomeHeader() {
   return (
-    <>
-      <div
-        style={{
-          display: 'flex',
-          justifyContent: 'space-between',
-          alignItems: 'center',
-        }}>
-        <div>
-          <h1 style={{ fontSize: '3rem', margin: '0 0.5rem 0 0' }}>
-            a space for mfers
-          </h1>
-        </div>
-        <div
-          style={{
-            alignSelf: 'flex-end',
-            fontSize: '2rem',
-            letterSpacing: '-0.05em',
-          }}>
-          <IconEmoji emoji="👇" alt="hand pointing down" /> check these mfers
-          out
-        </div>
+    <HomeHeaderWrapper>
+      <h1 className="header-title">a space for mfers</h1>
+      <div className="header-subtext">
+        <IconEmoji emoji="👇" alt="hand pointing down" /> check these mfers out
       </div>
-      <hr />
-    </>
+    </HomeHeaderWrapper>
   )
 }
 
